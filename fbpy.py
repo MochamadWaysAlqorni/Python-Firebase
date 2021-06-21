0570194414b68363679d2f193844c0440bf62fe5
MochamadWaysAlqorni <84778112+MochamadWaysAlqorni@users.noreply.github.com>
2021-06-21 19:12:34 +0700

    Update fbpy.py

--- fbpy.py
+++ fbpy.py
@@ -13,11 +13,11 @@ with open(tglf+'.csv', 'w', newline='') as file:
 while True:
     now = datetime.now()
     waktu = now.strftime("%H:%M:%S")
-    ikan = str(firebase.get('/DATA/ikan/', ''))
-    status = str(firebase.get('/DATA/status/', ''))
-    stoprq = str(firebase.get('/DATA/stop/', ''))
-    heater = str(firebase.get('/DATA/heater/', ''))
-    temp = str(firebase.get('/DATA/temp/', ''))
+    ikan = str(firebase.get('/DATA/ikan/', ''))  #yourdbchild
+    status = str(firebase.get('/DATA/status/', '')) #yourdbchild
+    stoprq = str(firebase.get('/DATA/stop/', '')) #yourdbchild
+    heater = str(firebase.get('/DATA/heater/', '')) #yourdbchild
+    temp = str(firebase.get('/DATA/temp/', '')) #yourdbchild
     with open(tglf+'.csv', 'a', newline='') as f:
         f.write(waktu+','+ikan+','+status+','+stoprq+','+heater+','+temp+',\n')
     print(waktu)
@@ -26,4 +26,4 @@ while True:
     print(stoprq)
     print(temp) 
     print("/")
-    time.sleep(120)
+    time.sleep(120) #repeat loop in 2min
